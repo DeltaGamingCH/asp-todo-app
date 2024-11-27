0ae7714f3c45075c49a40ed858c843dc174b414c
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-11-27 07:30:18 +0100

    fix(mock-data): update mock data ids

--- src/app/todo/mock-todo.ts
+++ src/app/todo/mock-todo.ts
@@ -2,6 +2,6 @@ import { ToDo } from "./todo";
 
 export const TODOS: ToDo[] = [
     { id: 1, name: 'Kaffeemaschine putzen', priority: 'high', complete: false, isEditing: false },
-    { id: 1, name: 'Test schreiben', priority: 'high', complete: false, isEditing: false },
-    { id: 1, name: 'Pflanzen giessen', priority: 'high', complete: false, isEditing: false }
+    { id: 2, name: 'Test schreiben', priority: 'high', complete: false, isEditing: false },
+    { id: 3, name: 'Pflanzen giessen', priority: 'high', complete: false, isEditing: false }
 ]
